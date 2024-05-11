de50cd0c87276c4b51911e2eb4b7d9d515927a57
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-05-11 10:05:10 -0400

    set up initial packages, files, and directories

---
+++ Client/Strats/src/App.tsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div id="root">
+        <h2>Welcome to Strats!</h2>
+      </div>
+    )
+  }
+}
+
+export default App
