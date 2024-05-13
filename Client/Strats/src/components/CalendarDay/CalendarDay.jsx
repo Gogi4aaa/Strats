4776ecad92027baacdeb5775ee75bfeb49aa3568
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-05-13 03:28:05 -0400

    more styling

--- Client/Strats/src/components/CalendarDay/CalendarDay.jsx
+++ Client/Strats/src/components/CalendarDay/CalendarDay.jsx
@@ -3,15 +3,14 @@ import './CalendarDay.scss';
 export default function CalendarDay({ date, data }) {
     return (
         <div key={date + Math.random()} className="calendar">
-            <hr />
             <div className='calendar-day'>{date}</div>
             {data.map(obj => {
                 return (
-                    <span key={date + ' ' + obj.time} className='calendar-hour'>
-                        <span>{obj.time}</span>
-                        <span>{obj.temp}({((obj.temp - 32) / 1.8).toFixed(2)})&deg;C</span>
+                    <div key={date + ' ' + obj.time} className='calendar-hour'>
+                        <span className='calendar-hour-data'>{obj.time}</span>
+                        <span className='calendar-hour-data'>{obj.temp}&deg;F({((obj.temp - 32) / 1.8).toFixed(2)})&deg;C</span>
                         {/* <span>{obj.code}</span> */}
-                    </span>
+                    </div>
                 )
             })}
         </div>
