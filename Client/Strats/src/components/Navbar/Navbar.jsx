be2851ba49f757a8a3d773a8aefb95ad388e0b4c
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-05-13 20:00:12 -0400

    ocd - added comment to navbar.jsx for adding routes

--- Client/Strats/src/components/Navbar/Navbar.jsx
+++ Client/Strats/src/components/Navbar/Navbar.jsx
@@ -15,9 +15,10 @@ export default function Navbar() {
         <>
             <i className={`fa-solid fa-bars nav-icon ${showMenu ? 'nav-icon-menu-hide' : 'nav-icon-menu-show'}`} onClick={clickHandler}></i>
             <i className={`fa-solid fa-x nav-icon ${showMenu ? 'nav-icon-close-show' : 'nav-icon-close-hide'}`} onClick={clickHandler}></i>
+
             <div className={`navbar-left ${showMenu ? 'navbar-left-show' : 'navbar-left-hide'}`}>
                 <Links linksContainer='ul' linksContainerClass="nav-list" links={
-                    <>
+                    <> // add routes here
                         <li className="nav-list-item">
                             <NavLink
                                 className={({isActive}) => isActive ? 'nav-list-link-active' : 'nav-list-link'}
