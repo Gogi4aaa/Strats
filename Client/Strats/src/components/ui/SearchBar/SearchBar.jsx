92c771b9d0b61eab4f78888536298d2f8e341cb9
Gogi4aaa <georgi-07@abv.bg>
2024-05-16 12:51:02 +0300

    add working autocomplete

--- Client/Strats/src/components/ui/SearchBar/SearchBar.jsx
+++ Client/Strats/src/components/ui/SearchBar/SearchBar.jsx
@@ -1,29 +1,79 @@
-import {   useState } from 'react';
-import '@geoapify/geocoder-autocomplete/styles/minimal.css';
-import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete';
+import { useEffect, useState } from "react";
+import "../SearchBar/SearchBar.css";
+const MIN_ADDRESS_LENGTH = 3;
+const DEBOUNCE_DELAY = -1000;
 const API_KEY = "341662abfefd4fe7a7d2121f1d802360";
 
 export default function SearchBar() {
-    const [currentLocation, setCurrentLocation] = useState('');
-    function onPlaceSelect() {
-        setCurrentLocation(4)
+  const [currentLocation, setcurrentLocation] = useState("");
+  const [data, setData] = useState([]);
+
+  function handleInputChange(e) {
+    setcurrentLocation(e.target.value);
+    console.log(currentLocation);
+  }
+  function handleAdressClick(result){
+    setcurrentLocation(result.formatted)
+    console.log(result.formatted)
+  }
+  function getLocations() {
+    if (currentLocation.length < MIN_ADDRESS_LENGTH) {
+      setData([]);
+      return false;
     }
-    function handleClick() {
-        console.log(5)
-        console.log(currentLocation)
-    }
-    return(
-        <>
-        <GeoapifyContext apiKey={API_KEY}>
-            <GeoapifyGeocoderAutocomplete
-            placeholder="Enter address here"
+    const currentTimeout = setTimeout(() => {
+      const promise = new Promise((resolve, reject) => {
+        var url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(
+          currentLocation
+        )}&format=json&apiKey=${API_KEY}`;
+        fetch(url).then((response) => {
+          // check if the call was successful
+          if (response.ok) {
+            response.json().then((data) => resolve(data));
+          } else {
+            response.json().then((data) => reject(data));
+          }
+        });
+      });
+      promise.then(
+        (data) => {
+          // here we get address suggestions
+          setData(data.results);
+          console.log(data)
+        },
+        (err) => {
+          if (!err.canceled) {
+            console.log(err);
+          }
+        }
+      );
+    }, DEBOUNCE_DELAY);
+    return () => {
+      clearTimeout(currentTimeout);
+    };
+  }
+  useEffect(() => {
+    getLocations();
+  }, [currentLocation]);
+  return (
+    <>
+      <div className="autocomplete-container" id="autocomplete-container">
+        <div className="input-container input-div">
+          <input
+            className="form-control boxed-left"
+            onChange={handleInputChange}
             value={currentLocation}
-            onUserInput={onPlaceSelect}
-            suggestionsChange={onPlaceSelect}
-            
-            />
-        </GeoapifyContext>
-            <button onClick={handleClick}>Test</button>
-        </>
-    )
+            type="text"
+            placeholder="Enter an address here"
+          />
+          <button className="search-button"><i className="fa-solid fa-magnifying-glass"></i></button>
+          <div className="autocomplete-items">
+            {data.map((result, index) => {
+              return <div key={index} onClick={() => handleAdressClick(result)}>{result.formatted}</div>;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
